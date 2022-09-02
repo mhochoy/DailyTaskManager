cfaf8572f49ae8918f3f03dbacbf434b1ec8989d
Hochoy M <51301524+mhochoy@users.noreply.github.com>
2022-09-01 20:48:16 -0400

    Bug fixes

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-
-from datetime import datetime
 from typing import TextIO
-
-import pytz
 
 from models.task import Task, Tasks
 import settings
@@ -15,36 +11,50 @@ def main():
     time = helpers.get_time()
     file_name = "My Daily Tasks"
     file_path = f"{settings.path}{file_name} {day}.txt"
-    file = open(file_path, "w")
+
     while not done:
+        settings.saved_tasks.update_log()
+        file = open(file_path, "w")
         with file:
             date_string = f"{day}\n{time}\n\n\n"
             file.write(date_string)
             print(date_string)
 
             print("Welcome to your Daily Task Manager. What do you want to do?")
-            decision = input("run/create/quit:   ")
-            handle_menu(decision, file)
+            decision = input("run/create/log/quit:   ")
+            done = handle_menu(decision, file)
 
-            print(settings.saved_tasks)
+        file.close()
 
-    file.close()
     input("Daily Task Log Finished. Press Enter to exit")
 
 
 def handle_menu(decision: str, f: TextIO):
     if decision == "run":
-        run(f=f)
+        if not settings.saved_tasks.completed:
+            run(f=f)
+            return False
+        else:
+            print("All tasks have been completed.\n\n")
+            return False
+
     if decision == "create":
         mission = input("What task do you want to complete?:    ")
         task: Task = Task(settings.saved_tasks.count() + 1, mission)
         settings.saved_tasks.add_task(task=task)
         settings.saved_tasks.update_log()
+        return False
+
+    if decision == "log":
+        print(settings.saved_tasks)
+        return False
+
     if decision == "quit":
-        done = True
+        return True
 
     else:
         print(f"'{decision}' isn't a valid command.\n")
+        return False
 
 
 def run(f: TextIO):
--- helpers.py
+++ helpers.py
@@ -5,9 +5,11 @@ import pytz
 
 def get_date():
     current_day = date.today()
+    return current_day
 
 
 def get_time():
     timezone = pytz.timezone('America/New_York')
     now = datetime.now(timezone)
     current_time = now.strftime("%I:%M:%S")
+    return current_time
